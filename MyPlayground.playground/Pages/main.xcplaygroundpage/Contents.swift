94ea35fbd4161c75f6dbb39d0dc848c0205616cb
TiciaChance <laticia.chance@flatironschool.com>
2016-07-05 14:58:16 -0400

    done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,8 +12,11 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func helloWorld() {
+    print("Hello World")
+}
 
-
+helloWorld()
 
 
 /*: question2
@@ -21,24 +24,33 @@
  */
 // write your code here
 
+func testingTesting123() {
+    let country = "England"
+    print("Can't wait to see what lies ahead for the future of \(country)")
+}
 
-
-
+testingTesting123()
 /*: question3
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
 
 
+func personsName(name:String)  {
+    print("Salut \(name)! How are you")
+}
 
-
+personsName("beyonce")
+personsName("jesse williams")
+personsName("kendrick lamar")
 /*: question4
  ### 4. Now call the function you wrote in Question 3 using a variable or constant instead of a string literal. What do you expect to see in the console? Try passing in a _variable_ you declared (using `var`) as an argument. Then change that variable's value and call your function again. What do you see in the console?
  */
 // write your code here
 
 
-
+let newName = "Luna Lovegood"
+personsName(newName)
 
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
@@ -46,7 +58,16 @@
 // write your code here
 
 
+func newFunction()  {
+    var harryPottercharacter = "Hermione Granger"
+    print("\(harryPottercharacter)")
+    
+    harryPottercharacter = "Ron Weasley"
+    print(harryPottercharacter)
+    
+}
 
+newFunction()
 
 /*:
  [Solution](solution)
